b4b48c14ee27a2e4739e5eb09c0899b5bbbd5d58
Morgane <morgane.julian@symbioz.io>
2020-10-09 18:20:56 +0200

    Initial Commit

---
+++ P03_01_XcodeRPGTests/P03_01_XcodeRPGTests.swift
@@ -0,0 +1,33 @@
+//
+//  P03_01_XcodeRPGTests.swift
+//  P03_01_XcodeRPGTests
+//
+//  Created by Symbioz on 09/10/2020.
+//
+
+import XCTest
+@testable import P03_01_XcodeRPG
+
+class P03_01_XcodeRPGTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
